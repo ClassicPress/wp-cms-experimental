ca5e1311ca63053109f0c22eb6cb8d7d7f4c072d
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-11-29 13:32:37 +0100

    Coding Standards: Add visibility to methods in `tests/phpunit/tests/`.
    Adds a `public` visibility to test fixtures, tests, data providers, and callbacks methods. This continues the previous efforts to make sure visibility is declared on all methods.
    
    Note: This will be enforced by WPCS 3.0.0.
    
    Follow-up to [51919], [52009], [52010].
    
    Props jrf.
    See #56791.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@54889 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -3274,7 +3274,7 @@ EOF;
 	 *
 	 * @param string $context
 	 */
-	function test_wp_get_loading_attr_default( $context ) {
+	public function test_wp_get_loading_attr_default( $context ) {
 		global $wp_query, $wp_the_query;
 
 		// Return 'lazy' by default.
@@ -3312,7 +3312,7 @@ EOF;
 		}
 	}
 
-	function data_wp_get_loading_attr_default() {
+	public function data_wp_get_loading_attr_default() {
 		return array(
 			array( 'the_content' ),
 			array( 'the_post_thumbnail' ),
@@ -3322,7 +3322,7 @@ EOF;
 	/**
 	 * @ticket 53675
 	 */
-	function test_wp_omit_loading_attr_threshold_filter() {
+	public function test_wp_omit_loading_attr_threshold_filter() {
 		global $wp_query, $wp_the_query;
 
 		$wp_query     = new WP_Query( array( 'post__in' => array( self::$post_ids['publish'] ) ) );
@@ -3354,7 +3354,7 @@ EOF;
 	/**
 	 * @ticket 53675
 	 */
-	function test_wp_filter_content_tags_with_wp_get_loading_attr_default() {
+	public function test_wp_filter_content_tags_with_wp_get_loading_attr_default() {
 		global $wp_query, $wp_the_query;
 
 		$img1         = get_image_tag( self::$large_id, '', '', '', 'large' );
