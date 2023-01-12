5c059532c102620be7192ec983fd87623e675183
Viktor Nagornyy <1692600+viktorix@users.noreply.github.com>
2023-01-12 14:12:58 -0500

    v1(pr1171): Update importer API
    Updated importer API endpoint from v1 PR #1171. WordPress Importer was not removed from the list since it will be compatible with an updated ClassicPress v2.

--- src/wp-admin/includes/import.php
+++ src/wp-admin/includes/import.php
@@ -149,7 +149,7 @@ function wp_get_popular_importers() {
 				'locale'  => $locale,
 				'version' => $wp_version,
 			),
-			'http://api.wordpress.org/core/importers/1.1/'
+			'https://api-v1.classicpress.net/core/importers/1.0/'
 		);
 		$options = array( 'user-agent' => 'WordPress/' . $wp_version . '; ' . home_url( '/' ) );
 
